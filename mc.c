955db099796f308ce460c6694c8aa99792d84a91
Dan Haugh <daniel.haugh@conning.com>
2014-07-10 13:08:45 +0000

    killed one bug and another popped up

--- mc.c
+++ mc.c
@@ -36,36 +36,34 @@ void setHeaderIndex(long long* headerAddr,int index){
 	*target = index;	
 }
 void setBodyIndex(long long* bodyAddr,int index){
-	int* target = (int*) (bodyAddr - 1);
-	*target = index;	
+	setHeaderIndex(bodyAddr - 1,index);
 }
 
 
 long long* readHeaderPointer(long long* headerAddr){
-	return *headerAddr;
+	return (long long*) *headerAddr;
 }
 long long* readBodyPointer(long long* bodyAddr){
-	return *(bodyAddr - 1);
+	return readHeaderPointer(bodyAddr - 1);
 }
 void setHeaderPointer(long long* headerAddr, long long* address){
-	*headerAddr = address;
+	*headerAddr = (long long) address;
 }
 void setBodyPointer(long long* bodyAddr, long long* address){
-	long long* target = bodyAddr - 1;
-	*target = address;
+	setHeaderPointer(bodyAddr - 1, address);
 }
 
 
 
 void* mc(int size){
+	
 	long long searching = size + 8;
 	// checks if I need to malloc more space
 	if(searching > LARGESTSIZE){
 		return malloc(2 * bs[NUMROWS - 1]);	
 	}
 	// finds the row in the ptrs table
-	int ptRow = -1;
-	int i;
+	int ptRow,i;
 	for(i = 0; i < NUMROWS; i++){
 		if (searching <= bs[i]){
 			ptRow = i;
@@ -76,22 +74,23 @@ void* mc(int size){
 	// if no more blocks of this size
 	if(pt[ptRow] == 0){
 		// 8 bytes into block to split
-		long long* toSplit = /*(long long*)*/mc(2 * size + 8);
+		long long* toSplit = (long long*)mc(2 * size + 8);
 		long long* startOfBlock = toSplit - 1;
-		long long* half = (long long*) ((char*) startOfBlock + bs[ptRow]);
-		//assign the corresponding index ptr to the 2nd half of black
-		*((int*) half) = ptRow; 
+		long long* half = (long long*) (((char*) startOfBlock) + bs[ptRow]);
+		//assign the corresponding index ptr to the 2nd half of block
+		setHeaderIndex(half,ptRow);
 		fr(half + 1); // free 8 bytes after given
+		// set header of the block to return to the corresponding row
+		setHeaderIndex(startOfBlock,ptRow);
 		return toSplit;	
 	} 
 
 
 	else{
 		long long* out = pt[ptRow];
-		//pt[ptRow] = (void*) *((double*)out);//*((void*) out);
-		//memcpy(&pt[ptRow], out, sizeof(void*)); // assign the next block
-		//pt[i] = *((double*)out); // assign the next block
-		*((int*) out) = i; // assign pt index to block
+		long long* nextOutAddr = readHeaderPointer(out);
+		pt[ptRow] =  nextOutAddr; // assign the next block
+		setHeaderIndex(out,ptRow); // assign pt index to block
 		return out + 1; // return 8 bytes after header
 	}
 
@@ -100,43 +99,52 @@ void* mc(int size){
 void fr(void* addr){
 	long long* startOfBlock = ((long long*) addr) - 1;
 	// index of pt row to add block to
-	int ptIndex = *((int*)startOfBlock);	
-
+	int ptIndex = readBodyIndex(addr);//*((int*)startOfBlock);	
 	// update the new Blocks next pointer	
-	//memcpy(startOfBlock,pt+ptIndex,sizeof(long long));
-	*startOfBlock = (long long)pt[ptIndex];
+	long long* toSet = pt[ptIndex];
+	setHeaderPointer(startOfBlock,pt[ptIndex]);
+	// update the pt list
 	pt[ptIndex] = startOfBlock;
 	
 }
 
 
 void fixedTest(int size){
-	printf("testing with size %d\n",size);
+	printf("\nSTART TEST ON SIZE %d\n\n",size);
 	int i,j;
 	int calls = 3;
 	int testSize = size;
 
 	long long** buffs = alloca(sizeof(long long*) * calls);
+	printf("begin malloc\n\n");
 	for(i = 0; i < calls; i++){
 		long long* al = mc(testSize);
+		printf("malloc'd at index %d: 0x%x\n",i,al);
 		memset(al, i + 'a', testSize);
 		buffs[i] = al;
+		// printf("Current header at index %d: %d\n",i,readBodyIndex(buffs[i]));
+		//assert(readBodyIndex(buffs[i]) == 0);
 	}
 	// BUG at this point, buffs[1] and buffs[2] are identical 
+	printf("\n\nprinting contents\n\n");
 	for(i = 0; i < calls; i++){
 		char* curr = (char*) buffs[i];
 		for(j = 0; j < testSize; j++){
-			//assert(curr[j] == i + 'a');	
+			assert(curr[j] == i + 'a');	
 		}
 		printf("%s\n",curr);
 		
 	}
 	
+	printf("\n\nbegin freeing buffs\n\n");
 	for(i = 0; i < calls;i++){
+		printf("freeing item %d at address 0x%x\n",i,buffs[i]);
+		printf("this block's next: 0x%x\n",pt[0]);
 		fr(buffs[i]);	
 		
+		
 	}
-	printf("\n\n");
+
 }
 
 
@@ -148,28 +156,53 @@ void printRows(){
 		int acc = 0;
 		void* curr = row;
 
+		printf("row %d: \t",i);
 		while(curr != 0){
-			acc++;
+			printf("%x\t",curr);
 		//TODO fix this	
 
 		}
-		printf("row %d: %d items",i,acc);
-		
+		printf("\n");	
 	}
 
 }
 int main(int argc, char* argv[]){
 	int i; 
+	int min,max,target;
 	init();
-	/*for(i = 8; i < 55; i++){
+
+	for(i = 0; i < 100; i++){
+		printf("\n");
+	}
+	if(argc == 2){
+		fixedTest(atoi(argv[1]));
+	}
+	else if(argc == 3){
+		int max = atoi(argv[2]);
+		for(i = atoi(argv[1]); i < max; i++){
+			fixedTest(i);	
+		}
+	}
+
+	/*
+	for(i = 9; i < 5700; i++){
 		fixedTest(i);
 	}
+	*/
+	printf("\n\nSUCCESS\n");
+	/*
 	for(i = 100; i < 102; i++){
 		fixedTest(i);
-	}*/
-//	fixedTest(20);
+	}
+*/
+	//fixedTest(9);
+	/*
 	long long* header = alloca(24);
 	long long* body = header +1;
-
+	
+	setHeaderIndex(header,33);
+	assert(readHeaderIndex(header) == readBodyIndex(body));
+	assert(readHeaderPointer(header) == readBodyPointer(body));
+	*/
 }
 
